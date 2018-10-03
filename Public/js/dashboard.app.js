9e56f40b1c2c832e301d1fdd0f6ccb5e1beef225
Jake McCarty <jwmccary@indiana.edu>
2018-10-03 17:26:06 -0400

    fetchTasks location change

--- Public/js/dashboard.app.js
+++ Public/js/dashboard.app.js
@@ -56,7 +56,7 @@ var dashboardApp = new Vue({
       }
     },
     fetchTasks () {
-      fetch('https://raw.githubusercontent.com/tag/iu-msis/dev/public/p1-tasks.json')
+      fetch('app/data/p1-tasks.json')
       .then( response => response.json() )
       .then( json => {dashboardApp.tasks = json} )
       .catch( err => {
