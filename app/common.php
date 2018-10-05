ec8ec3d8f7764b1012a7e4dfc2a2030d8d656403
Jake McCarty <jwmccary@indiana.edu>
2018-10-05 11:04:31 -0400

    removed debug code.

--- app/common.php
+++ app/common.php
@@ -4,7 +4,6 @@
 chdir(__DIR__);
 set_include_path (__DIR__);
 
-var_dump($_SERVER);
 if ($_SERVER['REQUEST_METHOD'] == 'POST'
 && stripos($_SERVER['CONTENT_TYPE'], 'application/json') !== false ) {
   $_POST = json_decode(file_get_contents('php://input'), true);
